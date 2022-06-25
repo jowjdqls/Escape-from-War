08ce773f26c460261cbb710c4e2f071e6adc1688
jojeongbin <chojb2004@gmail.com>
2022-06-25 22:06:00 +0900

    hpDown / Loadendscene

--- Assets/Scripts/hpba.cs
+++ Assets/Scripts/hpba.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class hpba : MonoBehaviour
 {    
@@ -29,13 +30,8 @@ public class hpba : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            curHp -= 10;
-            curhu -= 10;
-            curWa -= 10;
-            print("aaaaa");
-        }
+        HpManager();
+        Die();
         HandleHp();
     }
     
@@ -44,6 +40,63 @@ public class hpba : MonoBehaviour
         hpbar.value = (float)curHp / (float)maxHp;
         hugrybar.value = (float)curhu / (float)maxhu;
         waterbar.value = (float) curWa / (float) maxWa;
+    }
 
+    public void Die()
+    {
+        if(curHp <= 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+    
+    public void HpManager()
+    {
+        if(curhu >= 75)
+        {
+            curhu -= 0.002f;
+        }
+        else if(curhu >= 50)
+        {
+            curhu -= 0.005f;
+        }
+        else if(curhu >= 25)
+        {
+            curhu -= 0.008f;
+        }
+        else if(curhu >= 0)
+        {
+            curhu -= 0.01f;
+        }
+        
+        if(curWa >= 75)
+        {
+            curWa -= 0.002f;
+        }
+        else if(curWa >= 50)
+        {
+            curWa -= 0.005f;
+        }
+        else if(curWa >= 25)
+        {
+            curWa -= 0.008f;
+        }
+        else if(curWa >= 0)
+        {
+            curWa -= 0.01f;
+        }
+
+        if(50 >= curhu && curWa >= 25)
+        {
+            curHp -= 0.002f;
+        }
+        else if(25 >= curhu && curWa > 0)
+        {
+            curHp -= 0.005f;
+        }
+        else if(curhu <= 0 && curWa <=0)
+        {
+            curHp -= 0.008f;
+        }
     }
 }
