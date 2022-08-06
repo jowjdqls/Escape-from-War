279b59dc1a23a9ff320c308112f5f9f50ad7f462
jojeongbin <chojb2004@gmail.com>
2022-08-06 23:47:23 +0900

    FixItem

--- Assets/Scripts/Home/ItemDatabase.cs
+++ Assets/Scripts/Home/ItemDatabase.cs
@@ -1,6 +1,9 @@
+using System.Linq;
+using System.Security.Cryptography;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SystemRandom = System.Random;
 
 public class ItemDatabase : MonoBehaviour
 {
@@ -18,10 +21,28 @@ public class ItemDatabase : MonoBehaviour
 
     private void Start()
     {
-        for(int i = 0; i < 5; i++)
+        var used = new List<int>();
+        for(int i = 0; i < 21; i++)
         {
             GameObject go = Instantiate(fieldItemPrefab, pos[i], Quaternion.identity);
-            go.GetComponent<FieldItems>().SetItem(itemDB[Random.Range(0,20)]);
+            var index = randomExcept(20, used);
+            used.Add(index);
+            go.GetComponent<FieldItems>().SetItem(itemDB[index]);
         }
+    }
+
+    private static int randomExcept(int n, List<int> except) 
+    {
+        var x = except.OrderBy(i => i).ToArray();
+        var r = new SystemRandom();
+        int result = r.Next(n - x.Length);
+
+        for (int i = 0; i < x.Length; i++) 
+        {
+            if (result < x[i])
+                return result;
+            result++;
+        }
+        return result;
     }
 }
