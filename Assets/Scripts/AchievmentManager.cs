e376657f0219d73221347858d3200c84523128a2
jojeongbin <chojb2004@gmail.com>
2022-08-07 18:01:05 +0900

    achievmentUI

---
+++ Assets/Scripts/AchievmentManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievmentManager : MonoBehaviour
+{
+    public GameObject LockedUI1;
+    public GameObject LockedUI2;
+    public GameObject LockedUI3;
+    public GameObject LockedUI4;
+    public GameObject LockedUI5;
+    public GameObject LockedUI6;
+    public GameObject LockedUI7;
+    public GameObject LockedUI8;
+    public GameObject LockedUI9;
+    public GameObject LockedUI10;
+
+    public GameObject AchievUI1;
+    public GameObject AchievUI2;
+    public GameObject AchievUI3;
+    public GameObject AchievUI4;
+    public GameObject AchievUI5;
+    public GameObject AchievUI6;
+    public GameObject AchievUI7;
+    public GameObject AchievUI8;
+    public GameObject AchievUI9;
+    public GameObject AchievUI10;
+}
