b20ab3eb89a1d3fbaa3bb2f7e33274e965c63f94
jojeongbin <chojb2004@gmail.com>
2022-08-16 20:38:24 +0900

    achComment

--- Assets/Scripts/achivbtuManager.cs
+++ Assets/Scripts/achivbtuManager.cs
@@ -18,18 +18,83 @@ public class achivbtuManager : MonoBehaviour
     public GameObject ExTextUI9;
     public GameObject ExTextUI10;
 
-    public void Start()
-    {
-        ExUI.SetActive(false);
-    }
-
     public void closebtu()
     {
         ExUI.SetActive(false);
+        ExTextUI1.SetActive(false);
+        ExTextUI2.SetActive(false);
+        ExTextUI3.SetActive(false);
+        ExTextUI4.SetActive(false);
+        ExTextUI5.SetActive(false);
+        ExTextUI6.SetActive(false);
+        ExTextUI7.SetActive(false);
+        ExTextUI8.SetActive(false);
+        ExTextUI9.SetActive(false);
+        ExTextUI10.SetActive(false);
     }
 
     public void BackMain()
     {
         SceneManager.LoadScene("IntScene");
+    }
+
+    public void UI1btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI1.SetActive(true);
+    }
+
+    public void UI2btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI2.SetActive(true);
+    }
+
+    public void UI3btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI3.SetActive(true);
+    }
+
+    public void UI4btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI4.SetActive(true);
+    }
+
+    public void UI5btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI5.SetActive(true);
+    }
+
+    public void UI6btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI6.SetActive(true);
+    }
+
+    public void UI7btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI7.SetActive(true);
+    }
+
+    public void UI8btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI8.SetActive(true);
+    }
+
+    public void UI9btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI9.SetActive(true);
+    }
+
+    public void UI10btu()
+    {
+        ExUI.SetActive(true);
+        ExTextUI10.SetActive(true);
     }
 }
